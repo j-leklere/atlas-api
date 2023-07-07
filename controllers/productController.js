907460c9f8bf4e333fb643e8591de67c54e0bfc6
j-leklere <jleklere@yahoo.com.ar>
2023-07-07 07:59:23 -0300

    First commit - Database created, authorization and authentication done, error handling done, routes done

---
+++ controllers/productController.js
@@ -0,0 +1,80 @@
+const Product = require('./../models/productModel');
+const APIFeatures = require('./../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.getAllProducts = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Product.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const products = await features.query;
+
+  // SEND RESPONSE
+  res.status(202).json({
+    status: 'success',
+    results: products.length,
+    data: {
+      products,
+    },
+  });
+});
+
+exports.getProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  // product findOne({_id: req.params.id})
+
+  if (!product) {
+    return next(new AppError('No product found with that ID', 404));
+  }
+
+  res.status(202).json({
+    status: 'success',
+    data: {
+      product,
+    },
+  });
+});
+
+exports.createProduct = catchAsync(async (req, res, next) => {
+  const newProduct = await Product.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      product: newProduct,
+    },
+  });
+});
+
+exports.updateProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!product) {
+    return next(new AppError('No product found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      product,
+    },
+  });
+});
+
+exports.deleteProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+
+  if (!product) {
+    return next(new AppError('No product found with that ID', 404));
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
---
+++ models/productModel.js
@@ -0,0 +1,118 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+// const validator = require('validator');
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Un producto debe tener un nombre'],
+      unique: true,
+      trim: true,
+      maxlength: [
+        40,
+        'El nombre del producto debe tener 40 caracteres o menos',
+      ],
+      minlength: [5, 'El nombre del producto debe tener 5 caracteres o más'],
+    },
+    slug: String,
+    price: {
+      type: Number,
+      required: [true, 'Un producto debe tener un precio'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: 'El descuento debe ser menor que el precio regular',
+      },
+      default: 0,
+    },
+    size: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      trim: true,
+      required: [true, 'Un producto debe tener una categoría'],
+    },
+    stock: {
+      type: Number,
+      required: [true, 'Un producto debe indicar el stock'],
+      default: 0,
+    },
+    brand: {
+      type: String,
+      trim: true,
+      required: [true, 'Un producto debe tener una marca'],
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'Un producto debe tener una descripción'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'Un producto debe tener una imagen de portada'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+productSchema.virtual('discountedPrice').get(function () {
+  return this.price - this.priceDiscount;
+});
+
+productSchema.virtual('available').get(function () {
+  return this.stock > 0;
+});
+
+// productSchema.virtual('imageCoverUrl').get(function() {
+//   return `https://ejemplo.com/images/${this.imageCover}`;
+// });
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+productSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// QUERY MIDDLEWARE
+// productSchema.pre(/^find/, function (next) {
+//   this.find({ stock: { $gt: 0 } }); // Filtrar productos con stock mayor a 0
+
+//   next();
+// });
+
+productSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+// productSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({ $match: { stock: { $gt: 0 } } }); // Filtrar productos con stock mayor a 0
+
+//   console.log(this.pipeline());
+//   next();
+// });
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
